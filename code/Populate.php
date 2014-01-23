9e6243fd5166c02d22632a4b2039ed8e18d24a68
Will Rossiter <will.rossiter@gmail.com>
2014-01-23 16:22:26 +1300

    Initial commit of Populate from YAML

---
+++ code/Populate.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @package populate
+ */
+class Populate extends Object {
+		
+	/**
+	 * @config
+	 *
+	 * @var array
+	 */
+	private static $include_yaml_fixtures = array();
+
+	/**
+	 * @config
+	 *
+	 * An array of classes to clear from the database before importing. While
+	 * populating sitetree it may be worth clearing the 'SiteTree' table.
+	 *
+	 * @var array
+	 */
+	private static $truncate_objects = array();
+
+	/**
+	 * Flag to determine if we're already run for this session (i.e to prevent
+	 * parent calls invoking {@link requireRecords} twice).
+	 *
+	 * @var bool
+	 */
+	private static $ran = false;
+
+	/**
+	 * @var bool
+	 *
+	 * @throws Exception
+	 */
+	public static function requireRecords() {
+		if(self::$ran) {
+			return true;
+		}
+
+		self::$ran = true;
+
+		if(!Director::isDev()) {
+			throw new Exception('requireRecords can only be run in development environments');
+		}
+
+		$factory = Injector::inst()->create('PopulateFactory');
+
+		foreach(self::config()->get('truncate_objects') as $objName) {
+			// if the object has the versioned extension, make sure we delete
+			// that as well
+			foreach(DataList::create($objName) as $obj) {
+				if($obj->hasExtension('Versioned')) {
+					$obj->deleteFromStage('Live');
+				}
+
+				$obj->delete();
+			}
+		}
+
+		foreach(self::config()->get('include_yaml_fixtures') as $fixtureFile) {
+			$fixture = new YamlFixture($fixtureFile);
+			$fixture->writeInto($factory);
+		}
+
+		return true;
+	}
+}
---
+++ code/PopulateFactory.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @package populate
+ */
+class PopulateFactory extends FixtureFactory {
+
+	/**
+	 * Creates the object in the database as the original object will be wiped.
+	 *
+	 * @param string $class
+	 * @param string $identifier
+	 * @param array $data
+	 */
+	public function createObject($class, $identifier, $data = null) {
+		DB::alteration_message("Creating $identifier ($class)");
+		
+		if($data) {
+			foreach($data as $k => $v) {
+				if(preg_match('/^`(.)*`;$/', $v)) {
+					$str = substr($v, 1, -2);
+					$pv = null;
+
+					eval("\$pv = $str;");
+
+					$data[$k] =	$pv;
+				}
+			}
+		}
+
+		// if any merge labels are defined then we should create the object
+		// from that 
+		$lookup = null;
+
+		if(isset($data['PopulateMergeWhen'])) {
+			$lookup = DataList::create($class)->where(
+				$data['PopulateMergeWhen']
+			);
+		}
+		else if(isset($data['PopulateMergeMatch'])) {
+			$filter = array();
+
+			foreach($data['PopulateMergeMatch'] as $field) {
+				$filter[$field] = $data[$field];
+			}
+
+			if(!$filter) {
+				throw new Exception('Not a valid PopulateMergeMatch filter');
+			}
+
+			$lookup = DataList::create($class)->filter($filter);
+		}
+		else if(isset($data['PopulateMergeAny'])) {
+			$lookup = DataList::create($class);
+		}
+
+		if($lookup) {
+			$obj = $lookup->first();
+
+			foreach($lookup->limit(null, 1) as $old) {
+				if($old->hasExtension('Versioned')) {
+					$old->deleteFromStage('Live');
+				}
+
+				$old->delete();
+			}
+
+			$obj->update($data);
+			$obj->write();
+
+			if($obj) {
+				$this->fixtures[$class][$identifier] = $obj; 
+			}
+		}
+		else {
+			$obj = parent::createObject($class, $identifier, $data);
+		}
+
+		if($obj->hasExtension('Versioned')) {
+			$obj->publish('Stage', 'Live');
+			$obj->flushCache();
+		}
+
+		return $obj;
+	}
+}
