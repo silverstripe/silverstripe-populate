9b1ae230347b78dbde23453322dbee062abe36d4
Chris Penny <chris.penny@gmail.com>
2021-10-21 09:09:11 +1300

    Add PHP Codesniffer. Update PHPUnit to support 7.4

--- code/extensions/PopulateMySQLExport.php
+++ code/extensions/PopulateMySQLExport.php
@@ -18,84 +18,85 @@ use SilverStripe\ORM\DB;
  * Opt into this extension by adding this to your populate.yml configuration.
  *
  * <code>
- *	PopulateMySQLExportExtension:
- *	  export_db_path: ~/path.sql
+ *    PopulateMySQLExportExtension:
+ *      export_db_path: ~/path.sql
  *
- *	Populate:
- *	  extensions
- *	    - PopulateMySQLExportExtension
+ *    Populate:
+ *      extensions
+ *        - PopulateMySQLExportExtension
  * </code>
  *
  * @package populate
  */
-class PopulateMySQLExportExtension extends Extension {
-	use Configurable;
+class PopulateMySQLExportExtension extends Extension
+{
+    use Configurable;
 
-	/**
-	 * @config
-	 */
-	private static $export_db_path;
+    /**
+     * @config
+     */
+    private static $export_db_path;
 
-	public function getPath() {
-		$path = Config::inst()->get(__CLASS__, 'export_db_path');
+    public function getPath()
+    {
+        $path = Config::inst()->get(__CLASS__, 'export_db_path');
 
-		if(!$path) {
-			$path = Controller::join_links(TEMP_FOLDER . '/populate.sql');
-		} else {
-			$path = (substr($path, 0, 1) !== "/") ? Controller::join_links(BASE_PATH, $path) : $path;
-		}
+        if (!$path) {
+            $path = Controller::join_links(TEMP_FOLDER . '/populate.sql');
+        } else {
+            $path = (substr($path, 0, 1) !== "/") ? Controller::join_links(BASE_PATH, $path) : $path;
+        }
 
-		return $path;
-	}
+        return $path;
+    }
 
-	/**
-	 *
-	 */
-	public function onAfterPopulateRecords() {
-		$path = $this->getPath();
+    /**
+     *
+     */
+    public function onAfterPopulateRecords()
+    {
+        $path = $this->getPath();
 
-		DB::alteration_message("Saving populate state to $path", "success");
-		$result = DB::query('SHOW TABLES');
-		$tables = $result->column();
-		$return = '';
+        DB::alteration_message("Saving populate state to $path", "success");
+        $result = DB::query('SHOW TABLES');
+        $tables = $result->column();
+        $return = '';
 
-		foreach($tables as $table) {
-			$return.= 'DROP TABLE IF EXISTS `'.$table.'`;';
-			$row2 = DB::query("SHOW CREATE TABLE `$table`");
-			$create = $row2->nextRecord();
-			$create = str_replace("\"", "`", $create);
-			$return.= "\n\n".$create['Create Table'].";\n\n";
+        foreach ($tables as $table) {
+            $return .= 'DROP TABLE IF EXISTS `' . $table . '`;';
+            $row2 = DB::query("SHOW CREATE TABLE `$table`");
+            $create = $row2->nextRecord();
+            $create = str_replace("\"", "`", $create);
+            $return .= "\n\n" . $create['Create Table'] . ";\n\n";
 
+            $result = DB::query("SELECT * FROM `$table`");
 
-			$result = DB::query("SELECT * FROM `$table`");
-			while($row = $result->nextRecord()) {
-				$return.= 'INSERT INTO '.$table.' VALUES(';
+            while ($row = $result->nextRecord()) {
+                $return .= 'INSERT INTO ' . $table . ' VALUES(';
 
-				foreach($row as $k => $v) {
-					$v = addslashes($v);
-					$v = str_replace("\n", "\\n", $v);
+                foreach ($row as $k => $v) {
+                    $v = addslashes($v);
+                    $v = str_replace("\n", "\\n", $v);
 
-					if($v) {
-						$return.= '"'.$v.'"' ;
-					} else {
-						$return.= '""';
-					}
+                    if ($v) {
+                        $return .= '"' . $v . '"';
+                    } else {
+                        $return .= '""';
+                    }
 
-					$return.= ',';
-				}
+                    $return .= ',';
+                }
 
-				$return = rtrim($return, ',');
-				$return.= ");\n";
-			}
-		}
+                $return = rtrim($return, ',');
+                $return .= ");\n";
+            }
+        }
 
-		$return.="\n\n\n";
+        $return .= "\n\n\n";
 
+        $handle = fopen($path, 'w+');
 
-
-		$handle = fopen($path,'w+');
-
-		fwrite($handle, $return);
-		fclose($handle);
-	}
+        fwrite($handle, $return);
+        fclose($handle);
+    }
 }
--- tests/php/PopulateFactoryTest/PopulateFactoryTestVersionedObject.php
+++ tests/php/PopulateFactoryTest/PopulateFactoryTestVersionedObject.php
@@ -2,19 +2,20 @@
 
 namespace DNADesign\Populate\Tests\PopulateFactoryTest;
 
-use SilverStripe\ORM\DataObject;
 use SilverStripe\Dev\TestOnly;
+use SilverStripe\ORM\DataObject;
 use SilverStripe\Versioned\Versioned;
 
-class PopulateFactoryTestVersionedObject extends DataObject implements TestOnly {
+class PopulateFactoryTestVersionedObject extends DataObject implements TestOnly
+{
     private static $table_name = 'PopulateFactoryTestVersionedObject';
 
-    private static $db = array(
+    private static $db = [
         'Title' => 'Varchar',
-        'Content' => 'Varchar'
-    );
+        'Content' => 'Varchar',
+    ];
 
-    private static $extensions = array(
-        Versioned::class . '.versioned'
-    );
+    private static $extensions = [
+        Versioned::class . '.versioned',
+    ];
 }
